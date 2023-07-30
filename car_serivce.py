281104055062ce3f94d65e6b68f41ca95879e371
pdiddychickenpunk <pdiddychickenpunk@gmail.com>
2023-07-30 12:39:31 -0700

    Add Python file and header comments

---
+++ car_serivce.py
@@ -0,0 +1,8 @@
+# Author: Paul Adams
+
+# Date: 7/30/23
+
+# Description: This microservice recieves a JSON object within a request containing user choices
+# from the partner application. This service returns the most highly rated car from the external
+# dataset.
+
