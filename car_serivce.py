feeed43b33ad386a06591b347c2d54b50dc5b7fe
pdiddychickenpunk <pdiddychickenpunk@gmail.com>
2023-07-30 12:43:41 -0700

    Import csv library and add program attributions

--- car_serivce.py
+++ car_serivce.py
@@ -6,3 +6,25 @@
 # from the partner application. This service returns the most highly rated car from the external
 # dataset.
 
+from flask import Flask, render_template
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+
+
+
+"""Attributions:
+
+https://flexiple.com/python/python-get-current-directory/
+https://stackoverflow.com/questions/17211188/how-to-create-a-timer-on-python
+(second answer)
+https://docs.python.org/3/library/csv.html
+https://flask.palletsprojects.com/en/2.3.x/quickstart/#a-minimal-application
+
+"""
