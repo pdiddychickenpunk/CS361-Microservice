25ffaa052cc657afba60b3cbbb0786466eeb1787
pdiddychickenpunk <pdiddychickenpunk@gmail.com>
2023-07-30 12:56:22 -0700

    Add API route

--- car_serivce.py
+++ car_serivce.py
@@ -6,14 +6,19 @@
 # from the partner application. This service returns the most highly rated car from the external
 # dataset.
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
+
+@app.route("/get_car/<car_data>", methods=['POST'])
+def get_car(car_data):
+    data = request.json
+    return render_template('index.html', data)
 
 
 
@@ -26,5 +31,7 @@ https://stackoverflow.com/questions/17211188/how-to-create-a-timer-on-python
 (second answer)
 https://docs.python.org/3/library/csv.html
 https://flask.palletsprojects.com/en/2.3.x/quickstart/#a-minimal-application
+https://stackoverflow.com/questions/20001229/how-to-get-posted-json-in-flask
+(second answer)
 
 """
